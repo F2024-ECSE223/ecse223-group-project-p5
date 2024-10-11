fab7b4a03600899b169c27b258045f8b9cc4fda4
snowqc <romyburn0@gmail.com>
2024-10-10 21:02:32 -0400

    modified step definitions

--- src/test/java/ca/mcgill/ecse/coolsupplies/features/DeleteStudentStepDefinitions.java
+++ src/test/java/ca/mcgill/ecse/coolsupplies/features/DeleteStudentStepDefinitions.java
@@ -20,7 +20,7 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 public class DeleteStudentStepDefinitions {
 
-  private CoolSupplies coolSupplies;
+  private CoolSupplies coolSupplies=CoolSuppliesApplication.getCoolSupplies();
   private String error;
   private int errorCntr;
   // private List<Grade> gradeList = new ArrayList<>();
@@ -31,7 +31,9 @@ public class DeleteStudentStepDefinitions {
   // coolSupplies = CoolSuppliesApplication.getCoolSupplies();
   // }
 
-
+  /**
+   * @author Doddy Yang Qiu
+   */
   @Given("the following grade entities exists in the system \\(p5)")
   public void the_following_grade_entities_exists_in_the_system_p5(
       io.cucumber.datatable.DataTable dataTable) {
@@ -51,7 +53,9 @@ public class DeleteStudentStepDefinitions {
     // throw new io.cucumber.java.PendingException();
   }
 
-
+  /**
+   * @author Doddy Yang Qiu
+   */
   @Given("the following student entities exists in the system \\(p5)")
   public void the_following_student_entities_exists_in_the_system_p5(
       io.cucumber.datatable.DataTable dataTable) {
@@ -75,7 +79,9 @@ public class DeleteStudentStepDefinitions {
     }
   }
 
-
+  /**
+   * @author Brian Yang
+   */
   @When("the school admin attempts to delete from the system the student with name {string} \\(p5)")
   public void the_school_admin_attempts_to_delete_from_the_system_the_student_with_name_p5(
       String string) {
@@ -83,13 +89,18 @@ public class DeleteStudentStepDefinitions {
     callController(CoolSuppliesFeatureSet2Controller.deleteStudent(string));
   }
 
-
+  /**
+   * @author Brian Yang
+   */
   @Then("the number of student entities in the system shall be {string} \\(p5)")
   public void the_number_of_student_entities_in_the_system_shall_be_p5(String string) {
     // Write code here that turns the phrase above into concrete actions
     assertEquals(string, "" + coolSupplies.getStudents().size());
   }
 
+  /**
+   * @author Brian Yang
+   */
   @Then("the following student entities shall exist in the system \\(p5)")
   public void the_following_student_entities_shall_exist_in_the_system_p5(
       io.cucumber.datatable.DataTable dataTable) {
@@ -100,17 +111,20 @@ public class DeleteStudentStepDefinitions {
     // Double, Byte, Short, Long, BigInteger or BigDecimal.
     //
     // For other transformations you can register a DataTableType.
-    List<Map<String,String>> rows=dataTable.asMaps();
-    for (var row :rows) {
-       String name=row.get("name");
-       String gradeLevel=row.get("gradeLevel");
-       for (Student student : coolSupplies.getStudents()) {
-         assertEquals(name, student.getName());
-         assertEquals(gradeLevel,student.getGrade().getLevel());
-       }
+    List<Map<String, String>> rows = dataTable.asMaps();
+    for (var row : rows) {
+      String name = row.get("name");
+      String gradeLevel = row.get("gradeLevel");
+      for (Student student : coolSupplies.getStudents()) {
+        assertEquals(name, student.getName());
+        assertEquals(gradeLevel, student.getGrade().getLevel());
+      }
     }
   }
 
+  /**
+   * @author Brian Yang
+   */
   @Then("the error {string} shall be raised \\(p5)")
   public void the_error_shall_be_raised_p5(String string) {
     // Write code here that turns the phrase above into concrete actions
--- src/test/java/ca/mcgill/ecse/coolsupplies/features/GetStudentStepDefinitions.java
+++ src/test/java/ca/mcgill/ecse/coolsupplies/features/GetStudentStepDefinitions.java
@@ -17,24 +17,32 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class GetStudentStepDefinitions {
-  private CoolSupplies coolSupplies;
+  private CoolSupplies coolSupplies=CoolSuppliesApplication.getCoolSupplies();
   private TOStudent student;
   private List<TOStudent> students;
 
+  /**
+   * @author Brian Yang
+   */
   @When("the school admin attempts to get from the system the student with name {string} \\(p5)")
   public void the_school_admin_attempts_to_get_from_the_system_the_student_with_name_p5(
       String string) {
     // Write code here that turns the phrase above into concrete actions
-    student=CoolSuppliesFeatureSet2Controller.getStudent(string);
+    student = CoolSuppliesFeatureSet2Controller.getStudent(string);
   }
 
-
+  /**
+   * @author Brian Yang
+   */
   @When("the school admin attempts to get from the system all the students \\(p5)")
   public void the_school_admin_attempts_to_get_from_the_system_all_the_students_p5() {
     // Write code here that turns the phrase above into concrete actions
     CoolSuppliesFeatureSet2Controller.getStudents();
   }
 
+  /**
+   * @author Brian Yang
+   */
   @Then("the following student entities shall be presented \\(p5)")
   public void the_following_student_entities_shall_be_presented_p5(
       io.cucumber.datatable.DataTable dataTable) {
@@ -59,11 +67,13 @@ public class GetStudentStepDefinitions {
 
   }
 
-
+  /**
+   * @author Brian Yang
+   */
   @Then("no student entities shall be presented \\(p5)")
   public void no_student_entities_shall_be_presented_p5() {
     // Write code here that turns the phrase above into concrete actions
-     assertNull(student);
+    assertNull(student);
   }
 
 
