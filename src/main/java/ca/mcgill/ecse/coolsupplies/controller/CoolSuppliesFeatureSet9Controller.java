0d8b7949c63f39ca69f284f75a699f2cbc237139
sanadi3 <156261448+sanadi3@users.noreply.github.com>
2024-11-03 21:06:23 -0500

    draft 1 featureset

--- src/main/java/ca/mcgill/ecse/coolsupplies/controller/CoolSuppliesFeatureSet9Controller.java
+++ src/main/java/ca/mcgill/ecse/coolsupplies/controller/CoolSuppliesFeatureSet9Controller.java
@@ -4,19 +4,81 @@ import java.util.ArrayList;
 import java.util.List;
 import ca.mcgill.ecse.coolsupplies.application.CoolSuppliesApplication;
 import ca.mcgill.ecse.coolsupplies.model.CoolSupplies;
+import ca.mcgill.ecse.coolsupplies.model.InventoryItem;
 import ca.mcgill.ecse.coolsupplies.persistence.CoolsuppliesPersistence;
+import ca.mcgill.ecse.coolsupplies.model.Order;
+import ca.mcgill.ecse.coolsupplies.model.Order.Status;
+import ca.mcgill.ecse.coolsupplies.model.OrderItem;
 
 public class CoolSuppliesFeatureSet9Controller {
   private static CoolSupplies coolSupplies = CoolSuppliesApplication.getCoolSupplies();
 
   private CoolSuppliesFeatureSet9Controller() {}
-  
-  public static String updateOrderItem(String item ,String newQuantity,String orderNumber) {
-    throw new UnsupportedOperationException("Not implemented yet.");
-  }
-  
-  public static String deleteOrderItem(String item, String orderNumber) {
-    throw new UnsupportedOperationException("Not implemented yet.");
+
+  public static String updateOrderItem(String itemName, int newQuantity, int orderNumber) {
+
+    Order order = Order.getWithNumber(orderNumber);
+    if (order == null) {
+      return "Order " + orderNumber + " does not exist";
+    }
+
+    InventoryItem inventoryItem = InventoryItem.getWithName(itemName);
+    if (inventoryItem == null) {
+      return "Item " + itemName + " does not exist.";
+    }
+
+    OrderItem orderItem = null;
+    for (OrderItem oi : order.getOrderItems()) {
+      if (oi.getItem().getName().equals(itemName)) {
+        orderItem = oi;
+        break;
+      }
+    }
+
+    if (orderItem == null) {
+      return "Item " + itemName + " does not exist in the order " + orderNumber + ".";
+    }
+
+    try {
+      orderItem.setQuantity(newQuantity);
+      CoolsuppliesPersistence.save();
+    } catch (RuntimeException e) {
+      return e.getMessage();
+    }
+    return "";
   }
 
+  public static String deleteOrderItem(String itemName, int orderNumber) {
+
+    Order order = Order.getWithNumber(orderNumber);
+    if (order == null) {
+      return "Order " + orderNumber + " does not exist";
+    }
+
+    InventoryItem inventoryItem = InventoryItem.getWithName(itemName);
+    if (inventoryItem == null) {
+      return "Item " + itemName + " does not exist.";
+    }
+
+    OrderItem orderItem = null;
+    for (OrderItem oi : order.getOrderItems()) {
+      if (oi.getItem().getName().equals(itemName)) {
+        orderItem = oi;
+        break;
+      }
+    }
+
+    if (orderItem == null) {
+      return "Item " + itemName + " does not exist in the order " + orderNumber + ".";
+    }
+
+
+    try {
+      orderItem.delete();
+      CoolsuppliesPersistence.save();
+    } catch (RuntimeException e) {
+      return e.getMessage();
+    }
+    return "";
+  }
 }
