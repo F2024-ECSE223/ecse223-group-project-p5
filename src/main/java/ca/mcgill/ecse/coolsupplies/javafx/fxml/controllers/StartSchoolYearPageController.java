e0bbcde35a0e21043c8839a2625d226aa4c47d15
mouselzein <moustapha.elzein@mail.mcgill.ca>
2024-11-24 16:13:44 -0500

    grade Page completed, startSchoolYear fxml done but controller only has skeleton

---
+++ src/main/java/ca/mcgill/ecse/coolsupplies/javafx/fxml/controllers/StartSchoolYearPageController.java
@@ -0,0 +1,25 @@
+package ca.mcgill.ecse.coolsupplies.javafx.fxml.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import static ca.mcgill.ecse.coolsupplies.javafx.fxml.controllers.ViewUtils.successful;
+import ca.mcgill.ecse.coolsupplies.controller.CoolSuppliesFeatureSet12Controller;
+import ca.mcgill.ecse.coolsupplies.controller.TOGrade;
+import ca.mcgill.ecse.coolsupplies.javafx.fxml.CoolSuppliesFxmlView;
+import static ca.mcgill.ecse.coolsupplies.javafx.fxml.controllers.ViewUtils.callController;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+public class StartSchoolYearPageController {
+
+    @FXML
+    private Button startSchoolYearButton;
+
+    @FXML
+    void startSchoolYearClicked(ActionEvent event) {
+
+    }
+
+}
