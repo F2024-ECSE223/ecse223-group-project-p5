26a7aa63393f7d0522a3d3133197917e10298d68
JiaduoXing <jiaduo.xing@mail.mcgill.ca>
2024-11-27 11:35:57 -0500

    removed the code for fixing itempage

--- src/main/java/ca/mcgill/ecse/coolsupplies/javafx/fxml/controllers/ItemPageController.java
+++ src/main/java/ca/mcgill/ecse/coolsupplies/javafx/fxml/controllers/ItemPageController.java
@@ -93,9 +93,8 @@ public class ItemPageController {
         }
   
         else{
-          callController(CoolSuppliesFeatureSet3Controller.deleteItem(item.getName())){
-            CoolSuppliesFxmlView.getInstance().refresh();
-          }
+          callController(CoolSuppliesFeatureSet3Controller.deleteItem(item.getName()));
+        }
     }
 
    
